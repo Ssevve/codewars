8306d684573db9d2775dfc5d5618b04421576e1e
Seweryn <ssevve@gmail.com>
2022-07-22 23:39:09 +0200

    Add new kata (solved)

---
+++ 8-kyu/basic-training-add-item-to-an-array.js
@@ -0,0 +1,6 @@
+// https://www.codewars.com/kata/511f0fe64ae8683297000001
+
+websites.push('codewars');
+// websites[0] = 'codewars';
+// websites.unshift('codewars');
+// websites.splice(0,0,"codewars");
