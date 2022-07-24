e77009e67dd4256d46c7bbb086fcb6f3d186d02d
Seweryn <ssevve@gmail.com>
2022-07-24 16:19:07 +0200

    Add new kata (solved)

---
+++ 8-kyu/get-nth-even-number.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/5933a1f8552bc2750a0000ed
+
+function nthEven(n) {
+  return n * 2 - 2;
+}
+
+console.log(nthEven(1), 0, "Wrong Value!");
+console.log(nthEven(2), 2, "Wrong Value!");
+console.log(nthEven(3), 4, "Wrong Value!");
+console.log(nthEven(100), 198, "Wrong Value!");
+console.log(nthEven(1298734), 2597466, "Wrong Value!");
